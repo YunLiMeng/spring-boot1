38cbd8d8a61dd6eb41cc51491900c57d62842569
limeng <706031472@qq.com>
2019-08-30 18:07:03 +0800

    代码生成器

---
+++ src/main/java/com/example/demo/util/CodeGeneration.java
@@ -0,0 +1,76 @@
+package com.example.demo.util;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * @ author: limeng
+ * @ date: Created in 2019/8/30 17:44
+ * @ description：代码生成器
+ */
+public class CodeGeneration {
+
+    public static void main(String[] args) {
+
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+//        gc.setOutputDir("D://");//输出文件路径
+        gc.setAuthor("limeng");
+        gc.setOpen(false);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        mpg.setGlobalConfig(gc);
+
+        //自定义文件命名，注意%s 会自动填充表实体属性
+        gc.setControllerName("%sController");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        mpg.setGlobalConfig(gc);
+
+        //数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setUrl("jdbc:mysql://localhost:3306/ssmdb?serverTimezone=GMT%2B8");
+        mpg.setDataSource(dsc);
+
+        //策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setTablePrefix(new String[]{});//此处可以修改您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);//表名生成策略
+        strategy.setInclude(new String[]{"student"});//需要生成的表
+
+        strategy.setSuperServiceClass(null);
+        strategy.setSuperServiceImplClass(null);
+        strategy.setSuperMapperClass(null);
+
+        mpg.setStrategy(strategy);
+
+        //包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.demo");
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("serviceImpl");
+        pc.setMapper("mapper");
+        pc.setEntity("entity");
+        pc.setXml("xml");
+        mpg.setPackageInfo(pc);
+        System.out.println("=============start================");
+        //执行生成
+        mpg.execute();
+        System.out.println("=============end================");
+    }
+}
